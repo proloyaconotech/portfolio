b4c26933dab609f41f9d201b66222f8bf27e3d1a
Sebastian Loyacono <loyaconos@gmail.com>
2025-07-15 03:54:32 -0400

    project style change
    ADDS project style was replaced

--- assets/JS/scriptsprojects.js
+++ assets/JS/scriptsprojects.js
@@ -1,23 +1,5 @@
- // Basic Lightbox Viewer
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll(".project-gallery img");
-
-  images.forEach(img => {
-    img.addEventListener("click", () => {
-      const overlay = document.createElement("div");
-      overlay.className = "lightbox-overlay";
-      overlay.innerHTML = `
-        <div class="lightbox-container">
-          <img src="${img.src}" alt="${img.alt}">
-        </div>`;
-
-      document.body.appendChild(overlay);
-
-      overlay.addEventListener("click", (e) => {
-        if (e.target === overlay) {
-          overlay.remove();
-        }
-      });
-    });
+document.querySelectorAll('.project-gallery img').forEach(img => {
+  img.addEventListener('click', function() {
+    this.classList.toggle('expanded');
   });
 });
